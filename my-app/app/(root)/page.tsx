78ba36c551ffae69a89a5932d42d9e0f0e060578
ritesh <riteshsaini1601@gmail.com>
2025-04-25 18:30:26 +0530

    Hero Section implemented but working partially

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,7 +1,18 @@
+import '../globals.css';
 export default function Home() {
   return (
-      <>
-        HOME
-      </>
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup,<br/>
+                    Connect With Entrepreneurs
+                </h1>
+
+                <p className="sub-heading !max-w-3xl">
+                    Sbmit Ideas , Vore on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+            </section>
+        </>
+
   );
 }
