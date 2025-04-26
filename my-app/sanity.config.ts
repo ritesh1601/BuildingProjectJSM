4db0db750779b338d62b0eaaa17bae24df6693e1
ritesh <riteshsaini1601@gmail.com>
2025-04-26 15:33:52 +0530

    Sanity Schema Setup

--- my-app/sanity.config.ts
+++ my-app/sanity.config.ts
@@ -1,9 +1,8 @@
 'use client'
-
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
-
+import {markdownSchema} from 'sanity-plugin-markdown'
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
@@ -24,5 +23,6 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    markdownSchema(),
   ],
 })
---
+++ my-app/sanity/schemaTypes/startup.ts
@@ -0,0 +1,48 @@
+import { defineType, defineField } from "sanity";
+import { UserIcon } from "lucide-react";
+
+export const startup = defineType({
+    name: 'startup',
+    title: 'Startup',
+    type: "document",
+    fields: [
+        defineField({
+            name: "title",
+            type: "string",
+        }),
+        defineField({
+            name: "slug",
+            type: "slug",
+            options:{
+                source:'title'//'this is a great startup' -> 'this-is-a-great-startup-slug'
+            }
+        }),
+        defineField({
+            name: "author",
+            type: "reference",
+            to:{type:'author'}
+        }),
+        defineField({
+            name: "views",
+            type: "number",
+        }),
+        defineField({
+            name: "description",
+            type: "text",
+        }),
+        defineField({
+            name: "category",
+            type: "string",
+            validation:(Rule)=>Rule.min(1).max(20).required().error("Please Enter a Category"),
+        }), 
+        defineField({
+            name: "image",
+            type: "url",
+            validation:(Rule)=>Rule.required(),
+        }),
+        defineField({
+            name: "pitch",
+            type: "markdown",
+        }),
+    ],
+});
