28719ddf8a948c1bc472480679f910ba1d4538c4
ritesh <riteshsaini1601@gmail.com>
2025-04-28 00:42:29 +0530

    Sanity write Client setup which helps to make views count ppr and update at each refresh from the user

--- my-app/components/View.tsx
+++ my-app/components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from "@/components/ping";
 import {STARTUP_VIEWS_QUERY} from "@/lib/queries";
 import {client} from "@/sanity/lib/client";
+import {writeClient} from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({id}:{id:string}) => {
     const {views:totalViews}=await client.withConfig({useCdn:false}).fetch(STARTUP_VIEWS_QUERY,{id});
@@ -13,6 +15,11 @@ const View = async ({id}:{id:string}) => {
         );
     };
     // TODO : TO MODIFY THE NUMBER VIEWS ON THE EACH VISIT
+    after(async ()=>await writeClient
+        .patch(id)
+        .set({views:totalViews+1})
+        .commit()
+    )
     return (
         <div className="view-container">
             <div className="absolute -right-2 -top-2">
